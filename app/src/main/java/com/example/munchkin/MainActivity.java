aa5c955011314823e32fba965e9d42d2cf251a62
FreezyCGI <patrick.fritzl@gmx.at>
2021-05-02 01:08:38 +0200

    First Server-Client Application, which sends a message from Server to Client on the same device(localhost).

--- app/src/main/java/com/example/munchkin/MainActivity.java
+++ app/src/main/java/com/example/munchkin/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.munchkin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.minlog.Log;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    static TextView txtText;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtText = findViewById(R.id.txtText);
+
+        try {
+            new GameServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+            new GameClient();
+
+
+
+    }
+
+    public static void SetText(String text)
+    {
+        txtText.setText(text);
+    }
+    public static void AddText(String text)
+    {
+        txtText.setText(txtText.getText() + " " + text);
     }
 }
