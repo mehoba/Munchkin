59eaa69cc5ce1e8a82448a3d7ebe3371b7480ddf
mehoba <mehoba@edu.aau.at>
2021-05-02 12:17:33 +0200

    Added buttons for exit and to switch to dice activity

--- app/src/main/java/com/example/munchkin/MainActivity.java
+++ app/src/main/java/com/example/munchkin/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.munchkin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button exitBtn, diceBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        exitBtn= findViewById(R.id.exitBtn);
+        diceBtn= findViewById(R.id.diceBtn);
+
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(1);
+            }
+        });
     }
+
 }
