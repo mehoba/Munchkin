51b4135a480195259b3d779fe78361631504d7ad
mehoba <mehoba@edu.aau.at>
2021-05-02 12:51:26 +0200

    Random number generator when on click dicing between 1 and 6

--- app/src/main/java/com/example/munchkin/DiceActivity.java
+++ app/src/main/java/com/example/munchkin/DiceActivity.java
@@ -3,12 +3,39 @@ package com.example.munchkin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class DiceActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dice);
+
+        button= findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = getRandomNumber();
+                System.out.println(i);
+            }
+        });
+
+    }
+
+
+
+    public int getRandomNumber(){
+        Random rand= new Random();
+        return rand.nextInt(6-1)+1;
     }
 }
--- app/src/main/java/com/example/munchkin/MainActivity.java
+++ app/src/main/java/com/example/munchkin/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.munchkin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         exitBtn= findViewById(R.id.exitBtn);
         diceBtn= findViewById(R.id.diceBtn);
+
+        diceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DiceActivity.class);
+                startActivity(intent);
+            }
+        });
 
         exitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
