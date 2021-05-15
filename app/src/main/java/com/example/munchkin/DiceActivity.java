0786f455294957f6aa08495d687ca58bdca9e6f1
dgueler <d1gueler@edu.aau.at>
2021-05-15 20:03:41 +0200

    Added skip to countdown and a WinPopup

--- app/src/main/java/com/example/munchkin/DiceActivity.java
+++ app/src/main/java/com/example/munchkin/DiceActivity.java
@@ -9,10 +9,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.Random;
 
