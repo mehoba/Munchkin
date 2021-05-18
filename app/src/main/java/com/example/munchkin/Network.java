dbab02bf58a182cb4329ff57618d461a13d9a4ee
freezy <patrick.fritzl@gmx.at>
2021-05-19 01:01:31 +0200

    #60 Added classes from KryoDotNetTest

--- app/src/main/java/com/example/munchkin/Network.java
+++ app/src/main/java/com/example/munchkin/Network.java
@@ -1,20 +1,27 @@
 package com.example.munchkin;
 
-//import com.esotericsoftware.kryo.Kryo;
-//import com.esotericsoftware.kryonet.EndPoint;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.EndPoint;
 
 public class Network
 {
-//    static public final int port = 54555;
-//
-//    static  public  void register (EndPoint endpoint)
-//    {
-//        Kryo kryo = endpoint.getKryo();
-//        kryo.register(TestClass.class);
-//    }
-//
-//    static public class TestClass
-//    {
-//        public String text;
-//    }
+    static public final int port = 25565;
+    static public final String ipAdressServer = "192.168.178.1";
+
+    static  public  void register (EndPoint endpoint)
+    {
+        Kryo kryo = endpoint.getKryo();
+        kryo.register(TestClass.class);
+        kryo.register(PlayerName.class);
+    }
+
+    static public class TestClass
+    {
+        public String text;
+    }
+
+    static public class PlayerName
+    {
+        public  String playerName;
+    }
 }
