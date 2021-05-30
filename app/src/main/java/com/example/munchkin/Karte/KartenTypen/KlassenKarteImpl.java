8cff656489c9467cc6c3c2d1e7de7274df9a4895
lisa <lisa.gassinger@gmx.at>
2021-05-30 21:14:36 +0200

    KlassenKarte und KlassenKarteImpl in Kartentypen verändert

--- app/src/main/java/com/example/munchkin/Karte/KartenTypen/KlassenKarteImpl.java
+++ app/src/main/java/com/example/munchkin/Karte/KartenTypen/KlassenKarteImpl.java
@@ -2,7 +2,16 @@ package com.example.munchkin.Karte.KartenTypen;
 
 import com.example.munchkin.Karte.KarteImpl;
 
-public class KlassenKarteImpl extends KarteImpl {
+public class KlassenKarteImpl implements KlassenKarte {
+    private int image;
 
+    @Override
+    public int getImage() {
+        return image;
+    }
 
+    @Override
+    public void setImage(int image) {
+        this.image=image;
+    }
 }
