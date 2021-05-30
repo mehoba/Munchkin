6fa6f31667a2e12af0b62cacc58fc74e061eac42
lisa <lisa.gassinger@gmx.at>
2021-05-31 00:59:38 +0200

    Klasse Kampf erstellen um den Kampf zwischen Player und Monster auszuwerten

---
+++ app/src/main/java/com/example/munchkin/Kampf.java
@@ -0,0 +1,19 @@
+package com.example.munchkin;
+
+public class Kampf {
+    //ToDo: Funktionalität von Kämpfen implementieren
+    /*
+    * Soll sich Player suchen
+    * und Monster suchen
+    * Level und Ausrüstung von Player zusammenzählen
+    * Monsterlevel und eventuell darauf gespielte MonsterBuffs
+    * buffs die auf Monster oder Spieler gespielt werden
+    *
+    * Später weitere Monster die gespielt wurden
+    * später weitere Spiele, die helfen suchen
+    *
+    * und den kampf auswerten
+    *
+    * eventuell Weglaufen aufrufbar hier wenn monster stärker
+    * */
+}
