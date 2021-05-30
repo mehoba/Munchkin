5179f59352b10650579efd8cc0c971c84e758959
lisa <lisa.gassinger@gmx.at>
2021-05-31 01:19:28 +0200

    in Klasse Kampf funktionen defnieren und per Kommentar funktionalität erklären

--- app/src/main/java/com/example/munchkin/Kampf.java
+++ app/src/main/java/com/example/munchkin/Kampf.java
@@ -1,7 +1,9 @@
 package com.example.munchkin;
 
+import com.example.munchkin.Karte.KartenTypen.MonsterKarte;
+
 public class Kampf {
-    //ToDo: Funktionalität von Kämpfen implementieren
+    //ToDo: Klasse ausimplementieren
     /*
     * Soll sich Player suchen
     * und Monster suchen
@@ -14,6 +16,32 @@ public class Kampf {
     *
     * und den kampf auswerten
     *
-    * eventuell Weglaufen aufrufbar hier wenn monster stärker
+    *
     * */
+
+    public void kämpfen(){
+        //Soll vermutlich gestartet werden, sobald auf der UI der entsprechende Button gedrückt wurde
+        //ToDo Funktionalität implementieren
+        //Soll die Stärke des Player und die Stärke des Monster vergleichen
+        //Stärke des Player = Playerlevel + Ausrüstung + ev Buffs, die auf Player gespielt wurden
+        //Stärke Monster= Monsterlevel + ev. Monsterbuffs + ev Buffs, die auf Monster gespielt wurden
+        //Falls Player stärkter - rufe kampfGewonnnen() auf
+        //falls Monster stärker - rufe weglaufen() auf
+    }
+
+    public void kampfGewonnen(){
+        //wenn Player stärker ist als Monster wird diese Funktion aufgerufen
+        // ToDo:  Funktionalität implementieren
+        //es soll die getAnzahlSchätze auf Monster aufgerufen werden
+        //und die entsprechende Anzahl von Schatzkarten gezogen werden
+    }
+
+    public void weglaufen(){
+        //wenn monster stärker oder button in UI gedrückt wird diese Funktion aufgerufen
+        //ToDo:  Funktionalität implementieren
+        //ruft würfelfunktion auf
+        //Falls ergebnis des Würfels 5 oder 6 (>4) war weglaufen erfolgreich
+        //falls ergebnis 4 oder niedriger werden schlimmeDinge von Monster aufgerufen
+        }
+
 }
